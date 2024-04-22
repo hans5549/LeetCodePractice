b3d15285a8bc0c8d06830948240d7b4eeac4f5d7
hans.huang company-hp-laptop <s10323225@gmail.com>
2024-04-22 14:19:26 +0800

    Create LeetCode Problem 01 Solution

---
+++ Problem01-Two Sum/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApp1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] nums = [2, 7, 11, 15];
+            int target = 9;
+            var result = TwoSum(nums, target);
+
+            Console.WriteLine(result);
+        }
+
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return [i, j];
+                    }
+                }
+            }
+            return [];
+        }
+    }
+}
