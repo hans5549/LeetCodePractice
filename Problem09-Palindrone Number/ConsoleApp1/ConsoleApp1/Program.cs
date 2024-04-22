f4c61fd17e0727024d14cc4e4b3119ce2309ee5e
hans.huang company-hp-laptop <s10323225@gmail.com>
2024-04-22 14:48:30 +0800

    Create LeetCode Problem09 Solution

---
+++ Problem09-Palindrone Number/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+
+        public static bool IsPalindrome(int x)
+        {
+            string s = x.ToString();
+            int i = 0;
+            int j = s.Length - 1;
+            while (i < j)
+            {
+                if (s[i] != s[j])
+                {
+                    return false;
+                }
+                i++;
+                j--;
+            }
+
+            return true;
+        }
+
+        public static bool IsPalindrome2(int x)
+        {
+            if (x < 0) return false;
+            if (x / 10 == 0) return true;
+
+            var tmp = x;
+            var y = tmp % 10;
+            while (tmp / 10 != 0)
+            {
+                y = (y * 10) + (tmp /= 10) % 10;
+            }
+
+            return y == x ? true : false;
+        }
+    }
+}
